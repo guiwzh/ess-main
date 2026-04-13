9792281795a59eaec6598308ae2f1eeb23ebb908
Wangzhe Gui <21210720135@m.fudan.edu.cn>
2026-04-13 22:03:44 +0800

    feat: 添加eslint-plugin-unused-imports，自动移除未使用import
    - eslint --fix 时自动删除未引用的import
    - 未使用的变量以_开头可豁免

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import prettierPlugin from 'eslint-plugin-prettier'
 import prettierConfig from 'eslint-config-prettier'
+import unusedImports from 'eslint-plugin-unused-imports'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -23,10 +24,16 @@ export default defineConfig([
     },
     plugins: {
       prettier: prettierPlugin,
+      'unused-imports': unusedImports,
     },
-    // rules: {
-    //   'prettier/prettier': 'off',
-    // },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
+      ],
+    },
   },
   prettierConfig,
 ])

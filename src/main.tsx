170a0a60d4971e3a54d8630ab9b95065ece9d04c
Wangzhe Gui <21210720135@m.fudan.edu.cn>
2026-04-14 18:58:18 +0800

    fix: 去除 ProLayout header actions hover 遮罩

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,6 @@
-import { Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import './i18n'
 import './styles/global.less'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <Suspense fallback={<div>Loading...</div>}>
-    <App />
-  </Suspense>,
-)
+createRoot(document.getElementById('root')!).render(<App />)
